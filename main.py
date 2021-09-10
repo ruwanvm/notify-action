39abbec1ce69e8ba64c820f69b2cd490316720b8
Ruwan Mettananda <ruwanvm@gmail.com>
2021-09-10 18:18:04 +0530

    Integration test

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
     webhook = os.environ["INPUT_WEBHOOK"]
     message = os.environ['INPUT_MESSAGE']
     git_repo = os.environ['GITHUB_REPOSITORY']
-    repo_branch = os.environ['GITHUB_REF']
+    repo_branch = os.environ['GITHUB_HEAD_REF']
 
     encoded_message = {
-        'text': f"*Repo* : {git_repo} :x:\n*Branch* : {repo_branch}\n*Details* : {message}"
+        'text': f"*Repo* : {git_repo}\n*Branch* : {repo_branch}\n*Details* : {message}"
     }
     response = requests.post(webhook, data=json.dumps(encoded_message), headers={'Content-Type': 'application/json'})
 
