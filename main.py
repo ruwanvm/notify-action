a737fdf01cf189b0eb76d92ac6d48c93c4516d06
Ruwan Mettananda <ruwanvm@gmail.com>
2021-09-10 17:39:16 +0530

    Integration test

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+# import os
+# import requests
+# import json
+#
+#
+# def main():
+#     webhook = os.environ['INPUT_WEBHOOK']
+#     message = os.environ['INPUT_MESSAGE']
+#
+#     results = f"{message} is send to {webhook}"
+#
+#     message = {'text': message}
+#     response = requests.post(webhook, data=json.dumps(message), headers={'Content-Type': 'application/json'})
+#
+#     print(f"::set-output name=results::{results}")
+#
+#
+# if __name__ == "__main":
+#     main()
+
 import os
-import requests
-import json
+import requests  # noqa We are just importing this to prove the dependency installed correctly
 
 
 def main():
-    webhook = os.environ['INPUT_WEBHOOK']
-    message = os.environ['INPUT_MESSAGE']
+    my_input = os.environ["INPUT_WEBHOOK"]
 
-    results = f"{message} is send to {webhook}"
+    my_output = f"Hello {my_input}"
 
-    message = {'text': message}
-    response = requests.post(webhook, data=json.dumps(message), headers={'Content-Type': 'application/json'})
-
-    print(f"::set-output name=results::{results}")
+    print(f"::set-output name=myOutput::{my_output}")
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     main()
