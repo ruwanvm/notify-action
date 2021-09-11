b0e21e8c5d5c31817444fac1a59b41f257fc89dc
ruwanvm <ruwanvm@gmail.com>
2021-09-11 18:49:32 +0530

    Add teams crd format

--- main.py
+++ main.py
@@ -8,52 +8,38 @@ def main():
     message = os.environ['INPUT_MESSAGE']
     git_repo = os.environ['GITHUB_REPOSITORY']
     repo_branch = os.environ['GITHUB_REF']
+    user = os.environ['GITHUB_ACTOR']
+    job = os.environ['GITHUB_JOB']
+    run_no = os.environ['GITHUB_RUN_NUMBER']
 
-    encoded_message = {
-        "content": {
-            "title": "John Doe",
-            "facts": [
-                {
-                    "key": "Order Number",
-                    "value": "1234"
-                },
-                {
-                    "key": "Payment Method",
-                    "value": "VISA 5555-****"
-                }
-            ],
-            "items": [
-                {
-                    "title": "Data Transfer",
-                    "image": {
-                        "url": "https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png"
-                    },
-                    "price": "$ 38.45",
-                    "quantity": "368"
-                },
-                {
-                    "title": "App Service",
-                    "image": {
-                        "url": "https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png"
-                    },
-                    "price": "$ 45.00",
-                    "quantity": "720"
-                }
-            ],
-            "total": "$ 90.95",
-            "tax": "$ 7.50",
-            "buttons": [
-                {
-                    "type": "openUrl",
-                    "title": "More information",
-                    "image": "https://account.windowsazure.com/content/6.10.1.38-.8225.160809-1618/aux-pre/images/offer-icon-freetrial.png",
-                    "value": "https://azure.microsoft.com/en-us/pricing/"
-                }
-            ]
-        }
+    headers = {
+        'Content-Type': 'application/json'
     }
-    response = requests.post(webhook, data=json.dumps(encoded_message),
-                             headers={'Content-Type': 'application/vnd.microsoft.card.receipt'})
+
+    payload = {
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": "0076D7",
+        "summary": git_repo,
+        "sections": [{
+            "activityTitle": git_repo,
+            "activitySubtitle": f"On {repo_branch} - {job} ({run_no})",
+            "activityImage": "https://teamsnodesample.azurewebsites.net/static/img/image6.png",
+            "facts": [{
+                "name": "User",
+                "value": user
+            }, {
+                "name": "Message",
+                "value": message
+            }, {
+                "name": "Status",
+                "value": "Not started"
+            }],
+            "markdown": True
+        }]
+    }
+
+    response = requests.post(webhook, data=json.dumps(payload), headers=headers)
 
     output = f"{message} is send with status {response.status_code}"
 
